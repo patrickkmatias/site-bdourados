499c49de1ecabbfa7ce0c79fecddb3d1ada167bf
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-02 08:39:27 -0300

    agora o .json é criado eficientemente

--- admin/agende.php
+++ admin/agende.php
@@ -15,37 +15,13 @@ $query = $conexao->prepare("SELECT * FROM `servico` WHERE statusServico = 'ATIVO
 
 $query->execute();
 
-// [{"name":"John"}]
+$json = array();
 
-$json = "[";
+while ($r = $query->fetch(PDO::FETCH_ASSOC)) { // o $r é o nosso contador, cada vez que roda acrescenta em 1 - fetch() percorre toda a matriz
+    array_push($json , $r);
+}
 
-while ($r = $query->fetch()) { // o $r é o nosso contador, cada vez que roda acrescenta em 1 - fetch() percorre toda a matriz
-    
-    if($json != "["){       // primeiro ele pergunta se é diferente de [, que é o seu primeiro estado. ao passar pela primeira vez no loop seu estado já é diferente de [ e então aplica-se a ","
-        $json .= ",";       
-    }
-
-    $json .= '{"idServico":"' .$r["idServico"]. '",'; // PRIMEIRO 
-
-        $json .= '"nomeServico":"' .$r["nomeServico"]. '",'; // NOME 
-
-        $json .= '"descricaoServico":"' .$r["descricaoServico"]. '",';
-
-        $json .= '"valorServico":"' .$r["valorServico"]. '",'; // VALOR  
-
-        $json .= '"statusServico":"' .$r["statusServico"]. '",'; // VALOR  
-
-        $json .= '"dataCadServico":"' .$r["dataCadServico"]. '",'; // VALOR  
-
-        $json .= '"tempoExecServico":"' .$r["tempoExecServico"]. '",'; // VALOR  
-
-    $json .= '"idEmpresa":"' .$r["idEmpresa"]. '"}'; // ULTIMO 
-
-} // FIM DO LAÇO
-
-$json .= "]";
-
-echo $json
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
 /* até aqui, foi escrito na tela o que tem no banco */
 
 ?>
