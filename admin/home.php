f1bb7549bb5ce1c77faf74877900abd8bd865e41
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-13 23:08:46 -0300

    fix de semicolon, agora home.php abre

--- admin/home.php
+++ admin/home.php
@@ -3,11 +3,11 @@
 
 require_once("conexao.php");
 
-$conexao = Conexao::LigarConexao(); // agora a variavel $conexao recebe o metodo LigarConexao() da classe Conexao
+$conexao = Conexao::LigarConexao(); 
 
-$conexao->exec("SET NAMES utf8"); // exec executa um comando dentro do banco de dados
+$conexao->exec("SET NAMES utf8"); 
 
-if(!$conexao){ // verifica a conexão
+if(!$conexao){ 
     echo "Não foi possível conectar-se com o banco de dados!";
 }
 
@@ -23,7 +23,7 @@ $query = $conexao->prepare("
         funcionario
     LEFT JOIN portfolio 
     ON funcionario.idFuncionario = portfolio.idFuncionario
-"); // prepara uma ação do sql e armazena em $query
+");
 
 $query->execute();
 
@@ -31,23 +31,23 @@ $json = array();
 
 while ($r = $query->fetch(PDO::FETCH_ASSOC)) { // o $r é o nosso contador, cada vez que roda acrescenta em 1 - fetch() percorre toda a matriz
     array_push($json , $r);
-}
+};
 
 // segunda query para buscar o banner
 
-$query2 = $conexao->prepare("SELECT * FROM banner_app")
+$queryBanner = $conexao->prepare("SELECT * FROM banner_app");
 
-$query2->execute();
+$queryBanner->execute();
 
-$json2 = array();
+$jsonBanner = array();
 
-while ($r = $query2->fetch(PDO::FETCH_ASSOC)) {
-    array_push($json2, $r);
-}
+while ($r = $queryBanner->fetch(PDO::FETCH_ASSOC)) {
+    array_push($jsonBanner, $r);
+};
 
 echo json_encode($json, JSON_UNESCAPED_UNICODE);
 
-echo json_encode($json2, JSON_UNESCAPED_UNICODE);
+echo json_encode($jsonBanner, JSON_UNESCAPED_UNICODE);
 /* até aqui, foi escrito na tela o que tem no banco */
 
 ?>
