3c47276626da25aca226130bc663b37c95d6b9e8
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-14 22:49:23 -0300

    .php requisição para login feita

---
+++ admin/login.php
@@ -0,0 +1,43 @@
+<?php 
+
+require_once("conexao.php");
+
+$conexao = Conexao::LigarConexao(); 
+
+$conexao->exec("SET NAMES utf8"); 
+
+if(!$conexao){ 
+    echo "Não foi possível conectar-se com o banco de dados!";
+}
+
+if(isset($_GET['email']) || isset($_GET['senha'])) {
+    
+    
+$email = $_GET['email'];
+$senha = $_GET['senha'];
+
+$query = $conexao->prepare("SELECT * FROM cliente WHERE emailCliente = '$email' and senhaCliente = '$senha'" );
+
+$query->execute();
+
+$json = array();
+
+$dados = $query->fetch(PDO::FETCH_ASSOC);
+
+if(!$dados == false) {
+
+    $loginJson = array(
+        'msg' => array(
+            'logado'=>'Sim',
+            'texto'=>'Logado com sucesso'
+        ),
+        'Dados' => $dados
+    );
+
+    array_push($json , $loginJson);
+};
+
+echo json_encode($json, JSON_UNESCAPED_UNICODE);
+
+}
+?>
