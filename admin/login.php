1e898cbc7ab5ae92fc719712cb8d0e7933dec5b2
Patrick M <patrickmatias.silva@hotmail.com>
2022-06-19 00:16:56 -0300

    login com numero sem DDD agora é aceito

--- admin/login.php
+++ admin/login.php
@@ -1,32 +1,68 @@
 <?php 
 
+
+
 require_once("conexao.php");
+
+
 
 $conexao = Conexao::LigarConexao(); 
 
+
+
 $conexao->exec("SET NAMES utf8"); 
 
+
+
 if(!$conexao){ 
+
     echo "Não foi possível conectar-se com o banco de dados!";
+
 }
 
+
+
 if(isset($_GET['email']) || isset($_GET['senha'])) {
+
     
+
     
+
 $email = $_GET['email'];
+
 $senha = $_GET['senha'];
 
-$query = $conexao->prepare("SELECT * FROM cliente WHERE emailCliente = '$email' and senhaCliente = '$senha'" );
+if(is_numeric($email)) 
+    $query = $conexao->prepare("
+    SELECT
+        numeroFoneCliente, senhaCliente
+    FROM
+        fonecliente
+    JOIN cliente ON cliente.idCliente = fonecliente.idCliente
+    WHERE
+        numeroFoneCliente = $email && senhaCliente = '$senha'
+        ");
+    else 
+
+    $query = $conexao->prepare("SELECT * FROM cliente WHERE emailCliente = '$email' and senhaCliente = '$senha'" );
+
 
 $query->execute();
 
+
+
 $json = array();
+
+
 
 $dados = $query->fetch(PDO::FETCH_ASSOC);
 
-if(!$dados == false) {
+
+
+if($dados) {
 
     $loginJson = array(
+
         'msg' => array(
             'logado'=>'Sim',
             'texto'=>'Logado com sucesso'
@@ -35,9 +71,25 @@ if(!$dados == false) {
     );
 
     array_push($json , $loginJson);
+}else{
+
+    $loginJson = array(
+        "msg" => array(
+            'logado'=>'Não',
+            'texto'=>'Usuário inválido'
+        ),
+        "Dados" => $dados
+    );
+
+    array_push($json, $loginJson);
 };
+
+
 
 echo json_encode($json, JSON_UNESCAPED_UNICODE);
 
+
+
 }
+
 ?>
